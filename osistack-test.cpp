def51eb4711ac65bd7ef1aafdd6a36b5c4ef6694
Saotomych <alexey.avalon@gmail.com>
2016-03-11 21:25:15 +0300

    test compiles

---
+++ osistack-test.cpp
@@ -0,0 +1,393 @@
+/*
+ * osistack-test.cpp
+ *
+ *  Created on: 9 марта 2016 г.
+ *      Author: alex
+ */
+
+
+#include "osistack-test.h"
+
+const quint8 OsiStackTest::s_mmsInitRequestPDU[] = {
+			0xa8, 0x26,  0x80, 0x03, 0x00,  0xfd,
+			0xe8,  0x81, 0x01, 0x06,  0x82, 0x01, 0x06,  0x83, 0x01, 0x06,  0xa4, 0x16,
+			0x80, 0x01, 0x01,  0x81, 0x03, 0x05,  0xf1, 0x00,  0x82, 0x0c, 0x03,  0xee,
+			0x08, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x01,  0xef, 0x18
+};
+
+const quint8 OsiStackTest::s_testData[] = {
+			0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f,  0x00,  0x01,  0x02,  0x03,  0x04,  0x05,
+			0x06,  0x07,  0x08,  0x09,  0x0a,  0x0b,  0x0c,  0x0d,
+			0x0e,  0x0f
+};
+
+void OsiStackTest::startServer()
+{
+	OsiStackTest* pTest = OsiStackTest::getMainTest();
+
+	pTest->checkServerConnected = false;
+	pTest->checkServerErrorConnected = false;
+	pTest->checkServerErrorTransfer = false;
+	pTest->checkIllegalArg = false;
+	pTest->checkIllegalClassMbr = false;
+
+	// Create server and start listening
+	qint32 port = 18982;
+	pTest->pAcseAssociationListener = new CAcseAssociationListener;
+	pTest->pServerAcse = new CServerAcseSap(port, 0, QHostAddress("127.0.0.1"), pTest->pAcseAssociationListener, CSocketFactory::getSocketFactory());
+	pTest->pServerAcse = new CServerAcseSap(port, 0, QHostAddress("127.0.0.1"), nullptr, CSocketFactory::getSocketFactory());
+	pTest->pServerAcse->setMessageTimeout(500);
+	pTest->pServerAcse->setMessageFragmentTimeout(500);
+
+	// server slots
+	pTest->connect(pTest->pConnectionListener, SIGNAL(signalConnected(const CConnection*)), pTest, SLOT(slotServerClientConnected(const CConnection*)));
+	pTest->connect(pTest->pConnectionListener, SIGNAL(signalDisconnected(const CConnection*)), pTest, SLOT(slotServerClientDisconnected(const CConnection*)));
+	pTest->connect(pTest->pConnectionListener, SIGNAL(signalTSduReady(const CConnection*)), pTest, SLOT(slotServerTSduReady(const CConnection*)));
+	pTest->connect(pTest->pConnectionListener, SIGNAL(signalCRReady(const CConnection*)), pTest, SLOT(slotServerCRReady(const CConnection*)));
+	pTest->connect(pTest->pConnectionListener, SIGNAL(signalIOError(QString)), pTest, SLOT(slotServerIOError(QString)));
+
+	// acse server slots
+	pTest->connect(pTest->pAcseAssociationListener, SIGNAL(signalAcseConnected(CAcseAssociation*)), pTest, SLOT(slotAcseServerClientConnected(CAcseAssociation*)));
+	pTest->connect(pTest->pAcseAssociationListener, SIGNAL(signalAcseDisconnected(CAcseAssociation*)), pTest, SLOT(slotAcseServerAssociationClosed(CAcseAssociation*)));
+	pTest->connect(pTest->pAcseAssociationListener, SIGNAL(signalTSduReady(CAcseAssociation*)), pTest, SLOT(slotAcseServerTSduReady(CAcseAssociation*)));
+	pTest->connect(pTest->pAcseAssociationListener, SIGNAL(signalAcseCnReady(CAcseAssociation*)), pTest, SLOT(slotAcseServerCnReady(CAcseAssociation*)));
+	pTest->connect(pTest->pAcseAssociationListener, SIGNAL(signalAcseIOError(QString)), pTest, SLOT(slotAcseServerIOError(QString)));
+
+	pTest->pServerAcse->startListening();
+
+}
+
+void OsiStackTest::startClient()
+{
+	OsiStackTest* pTest = OsiStackTest::getMainTest();
+
+	pTest->checkClientConnected = false;
+	pTest->checkClientErrorConnected = false;
+	pTest->checkClientErrorTransfer = false;
+
+	// Create client and start connection
+	pTest->pClientAcse = new CClientAcseSap(CSocketFactory::getSocketFactory());
+
+	// client's error slots
+	pTest->connect(pTest->pClientAcse->m_pClientTSap, SIGNAL(signalIllegalArgument(QString)), pTest, SLOT(slotIllegalArgument(QString)));
+	pTest->connect(pTest->pClientAcse->m_pClientTSap, SIGNAL(signalConnectError(QString)), pTest, SLOT(slotConnectError(QString)));
+	pTest->connect(pTest->pClientAcse->m_pClientTSap, SIGNAL(signalIllegalClassMember(QString)), pTest, SLOT(slotIllegalClassMember(QString)));
+
+	pTest->pClientAcse->setMaxTPDUSizeParam(7);
+	pTest->pClientAcse->setMessageTimeout(5000);
+	pTest->pClientAcse->setMessageFragmentTimeout(5000);
+
+	QHostAddress address("127.0.0.1");
+	qint32 port = 18982;
+
+	pTest->pConnection = pTest->pClientAcse->m_pClientTSap->createConnection(address, port);
+	pTest->pAcseAssociation = pTest->pClientAcse->createAssociate(pTest->pConnection);
+
+	// connection slots
+	pTest->connect(pTest->pAcseAssociation, SIGNAL(signalAcseAssociationReady(CAcseAssociation*)), pTest, SLOT(slotAcseClientAssociationReady(CAcseAssociation*)));
+	pTest->connect(pTest->pAcseAssociation, SIGNAL(signalAcseTSduReady(CAcseAssociation*)), pTest, SLOT(slotAcseClientTSduReady(CAcseAssociation*)));
+	pTest->connect(pTest->pAcseAssociation, SIGNAL(signalAcseIOError(QString)), pTest, SLOT(slotAcseClientIOError(QString)));
+
+	QByteArray arrApdu( (char*) s_mmsInitRequestPDU, sizeof(s_mmsInitRequestPDU)/sizeof(s_mmsInitRequestPDU[0]) );
+	CBerByteArrayOutputStream apdu(arrApdu, 0);
+
+	pTest->pClientAcse->startAssociation(pTest->pAcseAssociation, QString(""), apdu);
+
+}
+
+void OsiStackTest::sendTestData(CConnection* that)
+{
+	QByteArray qdata;
+	qdata.reserve(sizeof(s_testData)/sizeof(s_testData[0]));
+	for (char c: s_testData)
+	{
+		qdata.push_back(c);
+	}
+
+	that->send(qdata, (quint32)0, qdata.size());
+}
+
+void OsiStackTest::Test1::runTest()
+{
+	qDebug() << "Test1: Check connection";
+
+	OsiStackTest* pTest = OsiStackTest::getMainTest();
+
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test1: checkClientConnected wrong", true, pTest->checkClientConnected);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test1: checkClientErrorConnected wrong", false, pTest->checkClientErrorConnected);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test1: checkServerConnected wrong", true, pTest->checkServerConnected);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test1: checkServerErrorConnected wrong", false, pTest->checkServerErrorConnected);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test1: checkIllegalArg wrong", false, pTest->checkIllegalArg);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test1: checkIllegalClassMbr wrong", false, pTest->checkIllegalClassMbr);
+}
+
+void OsiStackTest::Test2::runTest()
+{
+	qDebug() << "Test2: Check server side received data";
+
+	OsiStackTest* pTest = OsiStackTest::getMainTest();
+
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test2: Server side has error transfer", false, pTest->checkServerErrorTransfer);
+//
+//	qint32 expectedSize = sizeof(s_testData)/sizeof(s_testData[0]);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test2: Check server size of received data wrong", expectedSize, pTest->m_serverRcvData.size());
+//
+//	QByteArray::Iterator dataIt = pTest->m_serverRcvData.begin();
+//	for (char c: s_testData)
+//	{
+//		CPPUNIT_ASSERT_EQUAL_MESSAGE("Test2: Check server side received data wrong", c, *dataIt);
+//		dataIt++;
+//	}
+
+}
+
+void OsiStackTest::Test3::runTest()
+{
+	qDebug() << "Test3: Check client side received data";
+
+	OsiStackTest* pTest = OsiStackTest::getMainTest();
+
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test3: Client side has error transfer", false, pTest->checkClientErrorTransfer);
+//
+//	qint32 expectedSize = sizeof(s_testData)/sizeof(s_testData[0]);
+//	CPPUNIT_ASSERT_EQUAL_MESSAGE("Test3: Check client size of received data wrong", expectedSize, pTest->m_clientRcvData.size());
+//
+//	QByteArray::Iterator dataIt = pTest->m_clientRcvData.begin();
+//	for (char c: s_testData)
+//	{
+//		CPPUNIT_ASSERT_EQUAL_MESSAGE("Test3: Check client side received data wrong", c, *dataIt);
+//		dataIt++;
+//	}
+
+}
+
+// server slots
+void OsiStackTest::slotServerClientConnected(const CConnection*)
+{
+	qDebug() << "OsiStackTest::slotServerClientConnected";
+
+	checkServerConnected = true;
+}
+
+void OsiStackTest::slotServerClientDisconnected(const CConnection*)
+{
+	qDebug() << "OsiStackTest::slotServerClientDisconnected";
+
+	checkServerConnected = false;
+}
+
+void OsiStackTest::slotServerTSduReady(const CConnection* that)
+{
+
+}
+
+void OsiStackTest::slotServerCRReady(const CConnection*)
+{
+	qDebug() << "OsiStackTest::slotServerCRReady";
+
+}
+
+void OsiStackTest::slotServerIOError(QString str)
+{
+	qDebug() << "OsiStackTest::slotServerIOError: " << str;
+
+	checkServerErrorTransfer = true;
+}
+
+// server acse level slots
+void OsiStackTest::slotAcseServerClientConnected(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseServerAssociationClosed(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseServerCnReady(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseServerTSduReady(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseServerIOError(QString strErr)
+{
+	qDebug() << "OsiStackTest::slotAcseClientIOError: " << strErr;
+
+}
+
+
+// client slots
+void OsiStackTest::slotConnectionReady(const CConnection* that)
+{
+	qDebug() << "OsiStackTest::slotConnectionReady";
+
+	checkClientConnected = true;
+
+	CConnection* myconn = const_cast<CConnection*>(that);
+
+	OsiStackTest* pTest = OsiStackTest::getMainTest();
+
+	pTest->sendTestData(myconn);
+}
+
+void OsiStackTest::slotConnectionClosed(const CConnection*)
+{
+	qDebug() << "OsiStackTest::slotConnectionClosed";
+
+	checkClientConnected = false;
+}
+
+void OsiStackTest::slotClientTSduReady(const CConnection* that)
+{
+
+	if (checkClientConnected)
+	{
+		qDebug() << "OsiStackTest::slotClientTSduReady";
+
+		QByteArray tempBuffer;
+		if ( (const_cast<CConnection*>(that))->receive(tempBuffer) == true)
+		{
+			m_clientRcvData = tempBuffer;
+		}
+	}
+}
+
+void OsiStackTest::slotClientIOError(QString str)
+{
+	qDebug() << "OsiStackTest::slotClientIOError: " << str;
+
+	checkClientErrorTransfer = true;
+}
+
+// client acse slots
+void OsiStackTest::slotAcseClientAssociationReady(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseClientAssociationClosed(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseClientTSduReady(const CAcseAssociation*)
+{
+
+}
+
+void OsiStackTest::slotAcseClientIOError(QString strErr)
+{
+	qDebug() << "OsiStackTest::slotAcseClientIOError: " << strErr;
+
+}
+
+
+// Client Errors
+void OsiStackTest::slotIllegalArgument(QString strErr)
+{
+	qDebug() << "OsiStackTest::slotIllegalArgument: " << strErr;
+
+	checkIllegalArg = true;
+}
+
+void OsiStackTest::slotConnectError(QString strErr)
+{
+	qDebug() << "OsiStackTest::slotConnectError: " << strErr;
+
+	checkClientErrorConnected = true;
+}
+
+void OsiStackTest::slotIllegalClassMember(QString strErr)
+{
+	qDebug() << "OsiStackTest::slotIllegalClassMember: " << strErr;
+
+	checkIllegalClassMbr = true;
+}
+
+void OsiStackTest::prepare()
+{
+
+	test1 = new OsiStackTest::Test1("ACSE Association test");
+	test2 = new OsiStackTest::Test2("ACSE Send test");
+	test3 = new OsiStackTest::Test3("ACSE Receive test");
+
+	CServer* srv = new CServer(this);
+	CClient* clt = new CClient(this);
+
+	srv->start();
+	QThread::usleep(500);
+	clt->start();
+
+}
+
+void OsiStackTest::run()
+{
+    // Do processing here
+
+	CppUnit::TextTestRunner runner;
+	runner.addTest(test1);
+	runner.addTest(test2);
+	runner.addTest(test3);
+
+	runner.run();
+
+	std::ofstream outFile("testResult.xml");
+	CppUnit::XmlOutputter outputer(&runner.result(), outFile);
+	outputer.write();
+
+	emit finished();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    OsiStackTest *test = OsiStackTest::getMainTest(&a);
+
+    QObject::connect(test, SIGNAL(finished()), &a, SLOT(quit()));
+
+    QTimer::singleShot(0, test, SLOT(prepare()));
+    QTimer::singleShot(1000, test, SLOT(run()));
+
+    return  a.exec();
+}
---
+++ osistack-test.h
@@ -0,0 +1,222 @@
+/*
+ * osistack-test.h
+ *
+ *  Created on: 9 марта 2016 г.
+ *      Author: alex
+ */
+
+#ifndef OSISTACK_TEST_H_
+#define OSISTACK_TEST_H_
+
+#define UNITTEST
+
+#include <QCoreApplication>
+#include <QTimer>
+#include <QEventLoop>
+#include <QVector>
+
+#include <cppunit/TestCase.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/XmlOutputterHook.h>
+#include <cppunit/XmlOutputter.h>
+
+#include "servertsap.h"
+#include "clienttsap.h"
+#include "connection.h"
+#include "connectionlistener.h"
+#include "socketfactory.h"
+
+#include "AcseAssociation.h"
+#include "AcseAssociationListener.h"
+#include "ClientAcseSap.h"
+#include "ServerAcseSap.h"
+
+class OsiStackTest: public QObject
+{
+
+	Q_OBJECT
+
+	OsiStackTest(QObject *parent = 0) :
+		QObject(parent),
+		pServerAcse(nullptr),
+		pClientAcse(nullptr),
+		pConnection(nullptr),
+		pConnectionListener(nullptr),
+		pAcseAssociation(nullptr),
+		pAcseAssociationListener(nullptr),
+		checkClientConnected(false),
+		checkClientErrorConnected(false),
+		checkClientErrorTransfer(false),
+		checkServerErrorTransfer(false),
+		checkServerConnected(false),
+		checkServerErrorConnected(false),
+		checkIllegalArg(false),
+		checkIllegalClassMbr(false),
+		checkAcseAssociationReady(false),
+		checkAcseCnReady(false),
+		checkAcseIOError(false),
+		test1(nullptr),
+		test2(nullptr),
+		test3(nullptr)
+	{}
+
+public:
+
+	CServerAcseSap* pServerAcse;
+	CClientAcseSap* pClientAcse;
+	CConnection* pConnection;
+	CConnectionListener* pConnectionListener;
+	CAcseAssociation* pAcseAssociation;
+	CAcseAssociationListener* pAcseAssociationListener;
+
+	CBerOctetString whatIsString;
+
+	// Checks for transport level
+	bool checkClientConnected;
+	bool checkClientErrorConnected;
+	bool checkClientErrorTransfer;
+	bool checkServerErrorTransfer;
+	bool checkServerConnected;
+	bool checkServerErrorConnected;
+	bool checkIllegalArg;
+	bool checkIllegalClassMbr;
+
+	// Check for presentation level
+	bool checkAcseAssociationReady;
+	bool checkAcseCnReady;
+	bool checkAcseIOError;
+
+	QByteArray m_serverRcvData;
+	QByteArray m_clientRcvData;
+
+	static const quint8 s_mmsInitRequestPDU[];
+	static const quint8 s_testData[];
+
+	static OsiStackTest* getMainTest(QObject *parent = 0) {
+		static OsiStackTest* pthis = nullptr;
+		if (pthis == nullptr)
+		{
+			if (parent)
+				pthis = new OsiStackTest(parent);
+			else
+				throw std::invalid_argument("OsiStackTest* getMainTest: QObject is nullptr when singletone constructed");
+		}
+		return pthis;
+	}
+
+	// Start of server and client in QThreads
+	class CServer: public QThread
+	{
+		OsiStackTest* m_test;
+
+	public:
+		CServer(OsiStackTest *test): m_test(test) {}
+
+		void run()
+		{
+		    QObject::connect(m_test, SIGNAL(finished()), this, SLOT(quit()));
+
+		    m_test->startServer();
+
+			this->exec();
+		}
+	};
+
+	class CClient: public QThread
+	{
+		OsiStackTest* m_test;
+
+	public:
+		CClient(OsiStackTest *test): m_test(test){}
+
+		void run()
+		{
+		    QObject::connect(m_test, SIGNAL(finished()), this, SLOT(quit()));
+
+		    m_test->startClient();
+
+			this->exec();
+		}
+	};
+
+	// Test Classes
+	/*
+	 *  Class tests creation of connection client-server
+	 */
+	class Test1:public CppUnit::TestCase
+	{
+	public:
+		Test1(const std::string str): TestCase(str) {}
+		void runTest();
+	};
+
+	/*
+	 *  Class tests data sending from client to server
+	 */
+	class Test2:public CppUnit::TestCase
+	{
+	public:
+		Test2(const std::string str): TestCase(str) {}
+		void runTest();
+	};
+
+	/*
+	 *  Class tests data receiving from server to client
+	 */
+	class Test3:public CppUnit::TestCase
+	{
+	public:
+		Test3(const std::string str): TestCase(str) {}
+		void runTest();
+	};
+
+	Test1*	test1;
+	Test2*	test2;
+	Test3*	test3;
+
+public slots:
+
+	// server transport level slots
+	void slotServerClientConnected(const CConnection*);
+	void slotServerClientDisconnected(const CConnection*);
+	void slotServerTSduReady(const CConnection*);
+	void slotServerCRReady(const CConnection*);
+	void slotServerIOError(QString str);
+
+	// server acse level slots
+	void slotAcseServerClientConnected(const CAcseAssociation* that);
+	void slotAcseServerAssociationClosed(const CAcseAssociation*);
+	void slotAcseServerCnReady(const CAcseAssociation*);
+	void slotAcseServerTSduReady(const CAcseAssociation*);
+	void slotAcseServerIOError(QString strErr);
+
+	// client slots
+	void slotConnectionReady(const CConnection* that);
+	void slotConnectionClosed(const CConnection* that);
+	void slotClientTSduReady(const CConnection* that);
+	void slotClientIOError(QString str);
+
+	// client acse slots
+	void slotAcseClientAssociationReady(const CAcseAssociation*);
+	void slotAcseClientAssociationClosed(const CAcseAssociation*);
+	void slotAcseClientTSduReady(const CAcseAssociation*);
+	void slotAcseClientIOError(QString strErr);
+
+	// Client Errors
+	void slotIllegalArgument(QString strErr);
+	void slotConnectError(QString strErr);
+	void slotIllegalClassMember(QString strErr);
+
+    void prepare();
+    void run();
+	void startServer();
+	void startClient();
+
+	void sendTestData(CConnection* that);
+
+signals:
+    void finished();
+};
+
+
+#endif /* OSISTACK_TEST_H_ */
